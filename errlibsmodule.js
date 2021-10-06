6d73dd45a3af086e35ae57faf85ae08d84550b22
Mohammadali  Ghaliany <moalighaliany@gmail.com>
2021-10-06 10:16:24 +0300

    posh2

---
+++ errlibsmodule.js
@@ -0,0 +1,22 @@
+/**
+ * BACOM APP company :
+ * IOT Platform
+ * 
+ */
+
+/**
+ * With this module we will fix all err dependense 
+ */
+
+function Login_err(data){
+    /**
+     * check and contorol about passwords and login filds
+     */
+}
+
+
+/**
+ * We have exports all functions for use the other pages
+ */
+
+exports.Login_err=Login_err
